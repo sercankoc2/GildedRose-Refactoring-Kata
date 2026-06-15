c8f112e278913201e1a63fa8f0ef6412e978be12
Lux-i <lucjan.lubomski@gmail.com>
2026-06-15 15:42:05 +0200

    (doc) Take notes of existing functionality

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -13,10 +13,15 @@ public class GildedRose
 
     public void UpdateQuality()
     {
+        // For every Item in the list:
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (
+                Items[i].Name != "Aged Brie"
+                && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert"
+            )
             {
+                // Decrease the Quality of "normal" items by 1, if Quality is greater than 0
                 if (Items[i].Quality > 0)
                 {
                     if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
@@ -29,10 +34,13 @@ public class GildedRose
             {
                 if (Items[i].Quality < 50)
                 {
+                    // Increase Quality for every item by 1, if Quality is less than 50
                     Items[i].Quality = Items[i].Quality + 1;
 
+                    // If the item is a "Backstage pass":
                     if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
+                        // Increase by an additional 1 if SellIn is 10 or less
                         if (Items[i].SellIn < 11)
                         {
                             if (Items[i].Quality < 50)
@@ -41,6 +49,7 @@ public class GildedRose
                             }
                         }
 
+                        // Increase by an additional 1 if SellIn is 5 or less
                         if (Items[i].SellIn < 6)
                         {
                             if (Items[i].Quality < 50)
@@ -52,6 +61,7 @@ public class GildedRose
                 }
             }
 
+            // Decrease the SellIn by 1 for every item, except for "Sulfuras"
             if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
             {
                 Items[i].SellIn = Items[i].SellIn - 1;
@@ -67,17 +77,20 @@ public class GildedRose
                         {
                             if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                             {
+                                // Decrease Quality by 1 for every "normal" item if Quality is greater than 0
                                 Items[i].Quality = Items[i].Quality - 1;
                             }
                         }
                     }
                     else
                     {
+                        // Set Quality to 0 if item is a "Backstage pass"
                         Items[i].Quality = Items[i].Quality - Items[i].Quality;
                     }
                 }
                 else
                 {
+                    // Increase the Quality if the item is "Aged Brie" and Quality is less than 50
                     if (Items[i].Quality < 50)
                     {
                         Items[i].Quality = Items[i].Quality + 1;
